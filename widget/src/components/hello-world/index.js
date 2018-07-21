03858bd87ec463371be7191d52a3b43503fccf25
Prateek Bhatnagar <prateek89born@gmail.com>
2018-07-21 10:22:16 -0700

    removing extra demo folder

--- widget/src/components/hello-world/index.js
+++ widget/src/components/hello-world/index.js
@@ -2,10 +2,49 @@ import { h, Component } from "preact";
 import "./style.scss";
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.angles_ = [];
+    for (let angle = 0; angle < 360; angle+= 30) {
+      this.angles_.push(angle);
+    }
+  }
+  toRadians_(angle) {
+    return angle * (Math.PI / 180);
+  }
   render(props) {
     return (
       <div>
-        <h1 style={{ color: props.color }}>Hello, World!</h1>
+         <svg class="dial minues" width="400" height="400" viewBox="0 0 400 400">
+          <style>
+            {`:root{
+              fill: transparent;
+              stroke: #000
+            }
+            text {
+              fill: #000;
+              font-family: helvetica;
+            }
+            .hours{
+              display: none;
+            }`}
+          </style>
+          <circle cx="200" cy="200" r="190"/>
+          <g class="hours">
+            {
+              this.angles_.map((angle, index)=> {
+                return (<text x={160*Math.cos(this.toRadians_(angle)) + 200} y={160*Math.sin(this.toRadians_(angle)) + 200} class="digit" text-anchor="middle">{(index + 3) % 12 || 12}</text>);
+              })
+            }
+          </g>
+          <g class="minutes">
+            {
+              this.angles_.map((angle, index)=> {
+                return (<text x={160*Math.cos(this.toRadians_(angle)) + 200} y={160*Math.sin(this.toRadians_(angle)) + 200} class="digit" text-anchor="middle">{((index + 3) % 12 || 12) * 5}</text>);
+              })
+            }
+          </g>
+        </svg>
       </div>
     );
   }
