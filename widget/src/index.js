c499b67bd5d350b646f097b00ff1f7113e500179
Prateek Bhatnagar <prateek89born@gmail.com>
2018-07-22 19:44:51 -0700

    fixing component names

--- widget/src/index.js
+++ widget/src/index.js
@@ -3,7 +3,7 @@ let poly = require("preact-cli/lib/lib/webpack/polyfills");
 import { h } from "preact";
 import habitat from "preact-habitat";
 
-import Widget from "./components/hello-world";
+import Widget from "./components/my-component";
 
 let _habitat = habitat(Widget);
 
